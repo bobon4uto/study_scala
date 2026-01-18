0701dcdc92a61440bc02b03a88eb9c7edf1b6ee2
bobon4uto <vovapetrenko2101@gmail.com>
2026-01-18 23:38:25 +0700

    P27 is rough, i made the thing but not fully and it explodes on 4+ sized groups

--- scala3/P99/src/P27.scala
+++ scala3/P99/src/P27.scala
@@ -7,43 +7,67 @@
 // first idea is to just get all all all solutios and filter them to get only ones we need.
 // then generalization to x y z instead of 2 3 4
 // it slould be aeasy actually
+//
+//
+//
+//
+//List[(List[String], List[String])] (1 2 3, )
+//List[(List[String], List[String])] (2 3, 1)             (1 3, 2)
+//List[(List[String], List[String])] (2, 1 3) (3, 1 2)    (1, 2 3) (3, 2 1)
+//
+//
 import scala.util.Random
 
 
 object MainP {
 
-  def removeAt(n:Int, l:List[Any]):List[Any] = {
+  def removeAt(n:Int, l:List[String]):List[String] = {
       l.slice(0,n) :::l.slice(n+1,l.size)
   }
-  def rem1(l:List[Any]):List[(List[Any],Any)] = {
-    for {i <- 0 to l.size-1} yield { ( removeAt(i, l), l(i) ) }
-  }.toList
-  def remN(l:(List[Any], List[Any]) , n : Int):List[(List[Any], List[Any])] = {
-    println(l)
+  def rem1(l:List[(List[String], List[List[String]])], listn:Int ):List[(List[String], List[List[String]])] = {
+      for { subli <- 0 to l.size-1} yield {
+        for { index <- 0 to l(subli)(0).size-1} yield { val popped = l(subli)(0)(index)
+          val newl = removeAt(index, l(subli)(0))
+          val itsnotoldsitsoldss = l(subli)(1)
+          //println("!!!!!")
+          //println(itsnotoldsitsoldss)
+          //println("!!!!!")
+            val a:List[String] = List()
+          val olds = if (itsnotoldsitsoldss.size <= listn) { itsnotoldsitsoldss :+ a :+ a :+a } else {itsnotoldsitsoldss}
+          //println("?!!!!!")
+          //println(olds)
+          //println("?!!!!!")
+          val news = olds.updated(listn,olds(listn):+ popped)
+          (newl, news)
+        }
+      }.toList
+    }.toList.reduce(_:::_)
+  def remN(l:List[(List[String], List[List[String]])], n:Int,listn:Int ):List[(List[String], List[List[String]])] = {
     if (n>1) {
-      for {rem <- rem1(l(0))} yield { remN(rem,n-1)}
+      remN(rem1(l,listn), n-1, listn)
     } else {
-      for {rem <- rem1(l(0))} yield { rem }
+      rem1(l,listn)
     }
-  }.toList
+  }
   def group_rec(opt:List[Int],lin:List[String]) = {
     //for { i <- opt } yield {
-      remN((lin,List()), 3)
+      val a:List[List [String]] = List()
+        val iter1 = remN(List( (lin,a) ), opt(0), 0 )
+       // val curated1 = for (pos <- iter1) yield { (pos(0),a) }
+        val iter2 = remN(iter1, opt(1), 1 )
+       // val curated2 = for (pos <- iter2) yield { (pos(0),a) }
+        val iter3 = remN(iter2, opt(2), 2 )
+        //iter3
+       for (pos <- iter3) yield { pos(1) }
     //}
   }
   def group(opt:List[Int],lin:List[String]) = {
     group_rec(opt, lin)
   }
   @main def main() = {
-    println("""
-       group(List(2, 2, 5), List("Aldo", "Beat", "Carla", "David", "Evi", "Flip", "Gary", "Hugo", "Ida"))
-      """
-      )
-    println(
-       group(List(2, 3, 4), List("A", "B", "C", "D"))
-         //"E", "F", "G", "H", "I"))
-      )
+    println(group(List(1, 2, 3), List("A", "B", "C", "D", "E", "F")))
   }
+
 
 
 }
